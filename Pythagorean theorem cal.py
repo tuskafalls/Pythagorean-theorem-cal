5823825c6f34fedebf2186333b4e9f274a468ff1
tuska <tuskafalls@gmail.com>
2022-07-24 18:18:39 +0430

    first commit

---
+++ Pythagorean theorem cal.py
@@ -0,0 +1,8 @@
+# Pythagorean theorem calculator for hypotenuse side (c side)
+a = float(input('length of the side a: '))
+b = float(input('length of the side b: '))
+c = (a ** 2 + b ** 2) ** 0.5
+
+print(' ')
+print('the length of the side c is: ', c)
+
